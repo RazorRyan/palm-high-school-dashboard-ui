2d949b8cae7832b90b48b4ed40861a0c92e982c3
Ryan Chetty <ryan.chetty@psg.co.za>
2021-10-18 14:31:44 +0200

    updated package and server js

--- server.js
+++ server.js
@@ -19,19 +19,37 @@
 
 // console.log('Started');
 
-var express = require('express');
-var app = express();
-//app.use(requireHTTPS);
-var path = require('path');
+// var express = require('express');
+// var app = express();
+// //app.use(requireHTTPS);
+// var path = require('path');
 
-app.use(express.static(path.join(__dirname, '/dist/')));
+// app.use(express.static(path.join(__dirname, '/dist/')));
 
-app.get('/*', function(req, res) {
-  res.
+// app.get('/*', function(req, res) {
+//   res.
 
-sendFile(path.join(__dirname + '/dist/index.html'));
+// sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+// app.listen(8080, function () {
+//   console.log('App started');
+// })
+
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-app.listen(8080, function () {
-  console.log('App started');
-})
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
